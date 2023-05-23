0c17f6ae24e74b5cb421c0b8a09bc5a972c0231d
Indra <indra.ulziibadrakh@gmail.com>
2023-05-23 17:28:48 +0800

    vercel test

--- client/src/pages/aboutUs/index.js
+++ client/src/pages/aboutUs/index.js
@@ -1,5 +1,5 @@
-import AboutUs from '../../components/AboutUs';
-import { Layout } from '../layout';
+import AboutUs from "../../components/AboutUs";
+import { Layout } from "../../layouts/layout";
 
 export default function aboutUs() {
   // const props = [
