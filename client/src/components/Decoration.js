0738ed5563e33d9b2b3287acc571506ae0476e99
Indra <indra.ulziibadrakh@gmail.com>
2023-05-23 15:46:02 +0800

    Enhancement on banner

--- client/src/components/Decoration.js
+++ client/src/components/Decoration.js
@@ -6,31 +6,33 @@ function Decoration() {
     <div className="flex justify-between py-14 px-0 container flex-wrap">
       <div className=" max-md:w-full lg:w-5/12">
         <div className="text-lg text-blue-500 font-medium mb-3">
-          Learn how you want, where you want
+          Хүссэн газраасаа, хүссэн хичээлээ сур
         </div>
         <h1 className="text-[34px] font-bold mb-5 leading-snug">
-          The world’s largest selection of online courses
+          Цахим хичээл болон тэтгэлэгийн мэдээллийн нэгдсэн сан
         </h1>
         <span className="leading-loose">
-          Millions of people have used Kil-A-Bytes to decide which online course
+          Millions of people have used our website to decide which online course
           to take. We aggregate courses from many universities to help you find
           the best courses on almost any subject, wherever they exist. Our goal
           is to make online education work for everyone.
         </span>
         <div className="mt-7">
           <ul>
-            <div className="flex gap-7">
+            <div className="flex gap-[10px]">
               <li className="flex items-center flex-grow">
                 <span className="text-[40px] text-blue-500 ">
                   <BsPerson />
                 </span>
-                <div className="font-bold text-lg ml-2"> Private Classes</div>
+                <div className="font-bold text-lg ml-2">
+                  Ганцаарчилсан ангиуд
+                </div>
               </li>
               <li className="flex items-center flex-grow">
                 <span className="text-[40px] text-blue-500">
                   <BsLaptop />
                 </span>
-                <div className="font-bold text-lg ml-2">Small Groups</div>
+                <div className="font-bold text-lg ml-2">Уян хатан байдал</div>
               </li>
             </div>
             <div className="flex gap-7 mt-5">
@@ -38,13 +40,13 @@ function Decoration() {
                 <span className="text-[40px] text-blue-500">
                   <BsClock />
                 </span>
-                <div className="font-bold text-lg ml-2"> Lifetime Access</div>
+                <div className="font-bold text-lg ml-2">Насан туршийн</div>
               </li>
               <li className="flex items-center flex-grow">
                 <span className="text-[40px] text-blue-500">
                   <BsMouse2 />
                 </span>
-                <div className="font-bold text-lg"> Online Tutoring</div>
+                <div className="font-bold text-lg"> Цахим сургалт</div>
               </li>
             </div>
           </ul>
